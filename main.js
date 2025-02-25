57da22f711e2f6af103cdfd05a49e3255436c4fa
dayeDev <dazam1@naver.com>
2025-02-25 17:26:05 +0900

    과제 제출용 api 적용

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ const API_KEY = `07eb738ae18f4e42a9496b947b24b544`;
 let news = []
 const getLatestNews = async () => {
   const url = new URL(
-    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+     // 과제 제출용 api
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`
+
+    // 무료버전, 로컬에서만 api 가능
+    // `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+
   );
   const response = await fetch(url);
   const data = await response.json();
